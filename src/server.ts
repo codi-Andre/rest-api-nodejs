fb1a40b8377201aefe9cec8c5a6de536846fffdc
André Miranda <aegis.andre@gmail.com>
2023-08-06 22:15:02 -0300

    feat: create first table

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const table = await knex('sqlite_schema').select('*')
-  return table
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app.listen({ port: 3333 }).then(() => {
